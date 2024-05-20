457980d53c5d4558219e8b66fed2484e6320b441
Mohammed Kharoda <kharodawalam@gmail.com>
2024-05-20 21:14:20 +0530

    added separator and sheet

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icon/menu.svg"
+            alt="menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image src="/assets/logo.png" alt="logo" width={128} height={38} />
+          <Separator className="border-gray-50" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
 };
 
 export default MobileNav;
